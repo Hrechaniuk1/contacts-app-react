ae38cb876e34f4bcfb9814c30a157080aa2daee6
Alex Hrechaniuk <147260817+Hrechaniuk1@users.noreply.github.com>
2024-05-28 17:40:12 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,36 @@
+import { useSelector, useDispatch } from 'react-redux'
+import {useEffect} from 'react'
+
+
+import css from './App.module.css'
+import SearchBox from './SearchBox/SearchBox'
+import ContactForm from './ContactForm/ContactForm'
+import ContactList from './ContactList/ContactList'
+import { selectError, selectLoading } from '../redux/selectors'
+import {fetchContacts} from '../redux/contactsOps'
+
+// ------
+
+export default function App() {
+    const err = useSelector(selectError)
+    const load = useSelector(selectLoading)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchContacts())
+    }, [dispatch])
+
+
+    return (
+        <div>
+        <div className={css.main}>
+            <h1>Phonebook</h1>
+                <ContactForm/>
+            <SearchBox/>
+            </div>
+            {!err && load &&  <div>Loading...</div>}
+            <ContactList/>
+            
+        </div>
+    )
+}
